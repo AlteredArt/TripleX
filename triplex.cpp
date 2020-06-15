25a4e1a472962e4862f3f2fff50641aaea4b7803
Altered Art <cherokeedreemer@gmail.com>
2020-06-15 14:50:25 -0600

    finish triple x game

--- triplex.cpp
+++ triplex.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-
+#include <ctime>
 void PrintIntroduction(int Difficulty) {
     std::cout << "\n\nYou are an under cover agent infiltrating a level " << Difficulty;
     std::cout << " secret organization database... \nYou have a small window of time to access the mainframe..\n";
@@ -8,20 +8,19 @@ void PrintIntroduction(int Difficulty) {
 
 bool PlayGame(int Difficulty)
 {
-     PrintIntroduction(Difficulty);
+    PrintIntroduction(Difficulty);
     /*declaration expressions
     this is also multiline comment */
-    const int CodeA = 4;
-    const int CodeB = 6;
-    const int CodeC = 3;
+    const int CodeA = rand() % Difficulty + Difficulty;
+    const int CodeB = rand() % Difficulty + Difficulty;
+    const int CodeC = rand() % Difficulty + Difficulty;
 
-    int CodeSum = CodeA+CodeB+CodeC;
-    int CodeProduct = CodeA*CodeB*CodeC;
+    const int CodeSum = CodeA + CodeB + CodeC;
+    const int CodeProduct = CodeA *CodeB * CodeC;
 
-    std::cout << std::endl;
-    std::cout << "There are 3 digits that make up the code" <<std::endl;
-    std::cout << "The codes sum is equal to " << CodeSum << std::endl;
-    std::cout << "The codes product equals " << CodeProduct << std::endl;
+    std::cout << "+ There are 3 digits that make up the code";
+    std::cout << "\n+ The codes sum is equal to: " << CodeSum;
+    std::cout << "\n+ The codes product equals: " << CodeProduct << std::endl;
 
     int GuessA, GuessB, GuessC;
     std::cin >> GuessA >> GuessB >> GuessC;
@@ -32,30 +31,33 @@ bool PlayGame(int Difficulty)
     
     if ( GuessSum == CodeSum && GuessProduct == CodeProduct) 
     {
-        std::cout << "\nAccess Granted";
+        std::cout << "\nWell Done, you accessed one server, keep going";
         return true;
     } else {
-        std::cout << "\nWrong Code! ";
+        std::cout << "\nYou enetered the Wrong Code, careful and try again! ";
         return false;
     }
 }
 
 int main() 
 {
+    srand(time(NULL));
+    int const MaxDifficulty = 5;
     int LevelDifficulty = 1;
-   while(true)
+   while(LevelDifficulty <= MaxDifficulty)
    {
+    
        bool bLevelComplete = PlayGame(LevelDifficulty);
        std::cin.clear();    //this clears any errors
        std::cin.ignore();   //Discards the buffer
     
-        if (bLevelComplete)
+        if(bLevelComplete)
         {
             ++LevelDifficulty;
         }
-   
+       
    }
 
-
+     std::cout << "\n***Great work, You fully accessed all the servers and retrieved all the files***\n";
      return 0;
 }
